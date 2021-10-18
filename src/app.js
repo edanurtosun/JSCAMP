239fe99c0cdfbd0dac27d54f268d63e6a2a1d01c
edanurtosun <eedanurtosun@hotmail.com>
2021-10-19 00:02:17 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,30 @@
+console.log("Merhaba kodlama.ioo")
+
+//JS type safe değildir, int tanımlanan bir şeye string atabiliriz
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun=11  // hata verir -> euroDun is read-only
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi",
+"Emlak Konut Kredisi",
+"Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>");
+}
+console.log("</ul>")
+
+
+
+console.log(konutKredileri)
