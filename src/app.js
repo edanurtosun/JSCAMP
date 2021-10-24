6aec85270a6f0dedbb67f8bab83e301f492524c5
edanurtosun <eedanurtosun@hotmail.com>
2021-10-24 15:16:49 +0300

    Ödev 1 Notlar

--- src/app.js
+++ src/app.js
@@ -29,3 +29,67 @@ console.log("</ul>")
 
 
 console.log(konutKredileri)
+
+
+//Ödev 1
+//tip güvenli değil, bir değişkene string atıp sonrasında int atabiliriz.
+
+//var, let, const kullandığımız veri tipleridir.
+//sayi1 var olarak tanımladığında hata vermez.
+let sayi1 = 10
+ sayi1 = "engin demiroğ"
+let student = {id:1, name:"Engin"}
+console.log(student)
+
+function save(ogrenci, puan = 10) {
+    console.log(ogrenci.name + "  " + puan)
+}
+save(student, 100)
+
+let students = ["Engin Demiroğ",
+"Halit Kalaycı","Engin Toprak","Büşra"]
+
+console.log(students)
+
+let students2 = [student, {id:2, name:"Halit"},"Ankara", {city:"İstanbul"}]
+console.log(students2)
+
+//rest- geriye kalanlar(params, varArgs)
+let showProducts = function (id,...products) {
+    console.log(id)
+    console.log(products)
+    console.log(products[0])
+}
+
+//console.log(typeof showProducts)
+
+//showProducts(10,"Elma", "Armut", "Karpuz")
+
+//spread: ayrıştırmak
+let points = [1,2,3,4,50,4,60,14]
+console.log(...points)
+console.log(Math.max(...points))
+console.log(..."ABC","D",..."EFG","H")
+
+//Destructuring: elimizdeki değerleri değişkenlere atıyor
+let populations = [10000,20000,30000,[40000,100000]]
+let [small,medium,high,[veryHigh,maximum]] = populations
+console.log(small)
+console.log(medium)
+console.log(high)
+console.log(veryHigh)
+console.log(maximum)
+
+function someFunction([small1],number) {
+    console.log(small1)
+}
+someFunction(populations)
+
+let category = {id:1, name:"İçecek"}
+console.log(category.id)
+console.log(category["name"])
+
+let {id, name} = category
+console.log(id)
+console.log(name)
+
